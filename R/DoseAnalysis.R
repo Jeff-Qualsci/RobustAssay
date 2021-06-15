24cc060eb1ccd9602da3699483d38fc9405c131f
Jeff Weidner <jeff@qualsci.com>
2021-06-15 19:32:29 -0400

    Corrected Legends on Figure 6

--- R/DoseAnalysis.R
+++ R/DoseAnalysis.R
@@ -388,6 +388,12 @@ plot1 = ggplot (plot.results.Tgt1, aes (x=cutoff, y=Value, color=Result, linetyp
   ggtitle ("Target 1, N=4/Sample") + xlab("Activity Cutoff, %") + ylab("PPV or NPV")
 plot1
 
+T1N4ppv <- ggplot(plot.results.Tgt1, aes (x=cutoff, y=Value, color=Result, linetype=Scale)) +
+  geom_line(lwd=1.1) + theme_bw() + ylim (0.8, 1) +
+  ggtitle ("Tgt1, N=4/Sample") + xlab("Activity Cutoff, %") + ylab("PPV or NPV") +
+  theme_classic() +
+  theme(legend.position = 'none')
+
 ### Plot sensitivity and specificity results, Mean vs Median, for Tgt1
 
 plot.results.Tgt1 = pivot_longer (rbind (mean.results.Tgt1, median.results.Tgt1),
@@ -397,6 +403,12 @@ plot2 = ggplot (plot.results.Tgt1, aes (x=cutoff, y=Value, color=Result, linetyp
   geom_line(lwd=1.1) + theme_bw() + ylim (0.8, 1) +
   ggtitle ("Target 1, N=4/Sample") + xlab("Activity Cutoff, %") + ylab("Sensitivity or Specificity")
 plot2
+
+T1N4sel <- ggplot(plot.results.Tgt1, aes (x=cutoff, y=Value, color=Result, linetype=Scale)) +
+  geom_line(lwd=1.1) + theme_bw() + ylim (0.8, 1) +
+  ggtitle ("Tgt1, N=4/Sample") + xlab("Activity Cutoff, %") + ylab("Sensitivity or Specificity") +
+  theme_classic()
+theme(legend.position = 'none')
 
 ### Assay Tgt2, means
 
@@ -431,12 +443,17 @@ plot.results.Tgt2 = pivot_longer (rbind (mean.results.Tgt2, median.results.Tgt2)
 
 # Re-order PPV and NPV
 
-plot.results.Tgt2$Result = factor (plot.results.Tgt2$Result, levels = c("PPV", "NPV"))
+plot.results.Tgt2A$Result = factor (plot.results.Tgt2$Result, levels = c("PPV", "NPV"))
 
 plot3 = ggplot (plot.results.Tgt2, aes (x=cutoff, y=Value, color=Result, linetype=Scale)) +
   geom_line(lwd=1.1) + theme_bw() + ylim (0.8, 1) +
   ggtitle ("Target 2, N=4/Sample") + xlab("Activity Cutoff, %") + ylab("PPV or NPV")
 plot3
+
+T2N4ppv <- ggplot(plot.results.Tgt2, aes (x=cutoff, y=Value, color=Result, linetype=Scale)) +
+  geom_line(lwd=1.1) + theme_bw() + ylim (0.8, 1) +
+  ggtitle ("Target 2, N=4/Sample") + xlab("Activity Cutoff, %") + ylab("PPV or NPV") +
+  theme_classic()
 
 ### Plot sensitivity and specificity results, Mean vs Median, for Tgt2
 
@@ -447,6 +464,11 @@ plot4 = ggplot (plot.results.Tgt2, aes (x=cutoff, y=Value, color=Result, linetyp
   geom_line(lwd=1.1) + theme_bw() + ylim (0.8, 1) +
   ggtitle ("Target 2, N=4/Sample") + xlab("Activity Cutoff, %") + ylab("Sensitivity or Specificity")
 plot4
+
+T2N4sel <- ggplot(plot.results.Tgt2, aes (x=cutoff, y=Value, color=Result, linetype=Scale)) +
+  geom_line(lwd=1.1) + theme_bw() + ylim (0.8, 1) +
+  ggtitle ("Tgt2, N=4/Sample") + xlab("Activity Cutoff, %") + ylab("Sensitivity or Specificity") +
+  theme_classic()
 
 ## Put the four plots above together into one plot
 
@@ -497,6 +519,12 @@ plot5 = ggplot (plot.Indiv.Tgt1, aes (x=cutoff, y=Value, color=Result, linetype=
   ggtitle ("Target 1, N=1/Sample") + xlab("Activity Cutoff, %") + ylab("PPV or NPV")
 plot5
 
+T1N1ppv = ggplot(plot.Indiv.Tgt1, aes (x=cutoff, y=Value, color=Result, linetype=Scale)) +
+  geom_line(lwd=1.1) + theme_bw() + ylim (0.8, 1) +
+  ggtitle ("Tgt1, N=1/Sample") + xlab("Activity Cutoff, %") + ylab("PPV or NPV") +
+  theme_classic() +
+  theme(legend.position = 'none')
+
 ### Plot sensitivity and specificity results, Mean vs Median, for Tgt1
 
 plot.Indiv.Tgt1 = pivot_longer (rbind (Indiv.meanCtrl.Tgt1, Indiv.medianCtrl.Tgt1),
@@ -506,6 +534,12 @@ plot6 = ggplot (plot.Indiv.Tgt1, aes (x=cutoff, y=Value, color=Result, linetype=
   geom_line(lwd=1.1) + theme_bw() + ylim (0.8, 1) +
   ggtitle ("Target 1, N=1/Sample") + xlab("Activity Cutoff, %") + ylab("Sensitivity or Specificity")
 plot6
+
+T1N1sel <- ggplot(plot.Indiv.Tgt1, aes (x=cutoff, y=Value, color=Result, linetype=Scale)) +
+  geom_line(lwd=1.1) + theme_bw() + ylim (0.8, 1) +
+  ggtitle ("Tgt1, N=1/Sample") + xlab("Activity Cutoff, %") + ylab("Sensitivity or Specificity") +
+  theme_classic() +
+  theme(legend.position = 'none')
 
 ### Assay Tgt2, means
 
@@ -547,6 +581,11 @@ plot7 = ggplot (plot.Indiv.Tgt2, aes (x=cutoff, y=Value, color=Result, linetype=
   ggtitle ("Target 2, N=1/Sample") + xlab("Activity Cutoff, %") + ylab("PPV or NPV")
 plot7
 
+T2N1ppv = ggplot(plot.Indiv.Tgt2, aes (x=cutoff, y=Value, color=Result, linetype=Scale)) +
+  geom_line(lwd=1.1) + theme_bw() + ylim (0.8, 1) +
+  ggtitle ("Tgt2, N=1/Sample") + xlab("Activity Cutoff, %") + ylab("PPV or NPV") +
+  theme_classic()
+
 ### Plot sensitivity and specificity results, Mean vs Median, for Tgt2
 
 plot.Indiv.Tgt2 = pivot_longer (rbind (Indiv.meanCtrl.Tgt1, Indiv.medianCtrl.Tgt2),
@@ -557,6 +596,11 @@ plot8 = ggplot (plot.Indiv.Tgt2, aes (x=cutoff, y=Value, color=Result, linetype=
   ggtitle ("Target 2, N=1/Sample") + xlab("Activity Cutoff, %") + ylab("Sensitivity or Specificity")
 plot8
 
+T2N1sel <- ggplot(plot.Indiv.Tgt2, aes (x=cutoff, y=Value, color=Result, linetype=Scale)) +
+  geom_line(lwd=1.1) + theme_bw() + ylim (0.8, 1) +
+  ggtitle ("Tgt2, N=1/Sample") + xlab("Activity Cutoff, %") + ylab("Sensitivity or Specificity") +
+  theme_classic()
+
 ## Put the four plots above together into one plot
 
 plots.n1 = plot_grid (plot5, plot6, plot7, plot8, ncol=2)
@@ -564,4 +608,13 @@ plots.n1
 
 ggsave('Figures/N_1perSample.jpg', plot = plots.n1)
 
+T1ROC <- (T1N1sel/T1N1ppv/T1N4sel/T1N4ppv)
+
+T2ROC <- (T2N1sel/T2N1ppv/T2N4sel/T2N4ppv)
+
+Fig6 <- (T1ROC | T2ROC) +
+  plot_annotation(title = 'Figure 6. Assay Performance Measures', tag_levels = 'A') &
+  theme(plot.tag = element_text(face = 'bold'))
+
+ggsave('Figures/Weidner Fig 6.jpg', plot = Fig6, height = 8, width = 6, units = 'in', dpi = 300)
 
